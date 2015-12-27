d22e868efeb6967f8eb53cfbbaab564df9d6c071
unknown <unknown>
2015-12-27 19:57:49 +0000

    finished loadLSIBWVS

--- projfd/appfd/scripts/loadLSIBWVS.py
+++ projfd/appfd/scripts/loadLSIBWVS.py
@@ -5,7 +5,6 @@ from django.contrib.gis.geos import MultiPolygon, Polygon
 import os, urllib, zipfile
 from urllib import urlretrieve
 from bnlp import trim_location
-#wget 'http://geonode.state.gov/geoserver/wfs?format_options=charset%3AUTF-8&typename=geonode%3AAfricaAmericas_LSIB_Polygons_Simplified_2015&outputFormat=SHAPE-ZIP&version=1.0.0&service=WFS&request=GetFeature' -O 'AFRICA_AMERICAS.zip'
 
 def run():
 
@@ -28,36 +27,46 @@ def run():
         ds = DataSource(path_to_shp)
         print "ds is", ds
         for feature in ds[0]:
+            place = None
             name = trim_location(feature.get("CNTRY_NAME"))
-            #print feature.fields
-            #break
-            print 'name is', name
             #country_code = feature.get("CNTRY_CODE")
             country_code = feature.get("iso_alpha2")
             qs = Place.objects.filter(country_code=country_code, admin_level=0)
             count = qs.count()
             if count == 0:
-                unfound.append((name,country_code))
-                #qs = Place.objects.filter(name__startswith=name)
-            #count = qs.count()
-            #print "count for namestartswith", name, "is", count
+                qs = Place.objects.filter(admin_level=0, name__startswith=name)
+                count = qs.count()
+                if count == 0:
+                    qs = Place.objects.filter(name__startswith=name)
+                    count = qs.count()
+                    if count == 0:
+                        unfound.append((name,country_code))
+                    elif count == 1:
+                        #maybe add something in later that checks if distance from poly to point is sane
+                        found.append((name,country_code)) 
+                        place = qs[0]
+                    else:
+			unfound.append((name,country_code))
+                elif count == 1:
+                    place = qs[0]
+		    found.append((name,country_code))
+                else:
+		    unfound.append((name,country_code))
+
             elif count == 1:
                 found.append((name,country_code)) 
                 place = qs[0]
-            #place.mpoly = MultiPolygon([feature.geom])
+            else:
+                found.append((name,country_code))
+
+            if place:
                 geom = feature.geom.geos
                 if isinstance(geom, Polygon):
                     place.mpoly = MultiPolygon([geom])
                 elif isinstance(geom, MultiPolygon):
                     place.mpoly = geom
-                else:
-                    print "name is", name
                 place.save()
-            elif count > 1:
-                print "more than 1", name, country_code
-                found.append((name,country_code))
-
-    print "found = ", found
-    print "\n\n"
+                print "saved new polygon to ", place
+ 
     print "unfound = ", unfound
    
