e1612554c07c2917dd5316705ab67c57072fab8f
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2016-04-17 17:04:38 -0500

    Update load_geonames.sql

--- load_geonames.sql
+++ load_geonames.sql
@@ -1,7 +1,6 @@
-CREATE EXTENSION IF NOT EXISTS file_fdw;
+CREATE EXTENSION file_fdw;
 CREATE SERVER geoname_server FOREIGN DATA WRAPPER file_fdw;
-DROP FOREIGN TABLE appfd_geoname;
-CREATE FOREIGN TABLE IF NOT EXISTS appfd_geoname (
+CREATE FOREIGN TABLE appfd_geoname (
             geonameid integer,
             name varchar(200),
             asciiname varchar(200),
@@ -25,18 +24,4 @@ CREATE FOREIGN TABLE IF NOT EXISTS appfd_geoname (
         SERVER geoname_server
         OPTIONS ( filename '/tmp/allCountries.txt', format 'text' );
 
-TRUNCATE appfd_place CASCADE;
 INSERT INTO appfd_place (geonameid, name, point, population, country_code, admin1_code, admin2_code) SELECT geonameid, name, ST_SetSRID(ST_POINT(longitude, latitude), 4326), population, country_code, admin1_code, admin2_code FROM appfd_geoname;
-
-
--- indexes database for faster searching
--- commenting out for now, because even with indexing, full text searching is slow
---ALTER TABLE appfd_place ADD COLUMN textsearchable_index_col tsvector;
---UPDATE appfd_place SET textsearchable_index_col =
---     to_tsvector('english', name);
---CREATE INDEX textsearch_idx ON appfd_place USING gin(textsearchable_index_col);
-
--- this should only take about 5 minutes
-DROP INDEX IF EXISTS name_index;
-CREATE INDEX name_index ON appfd_place (name);
-
