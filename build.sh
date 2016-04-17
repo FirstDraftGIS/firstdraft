a42ff5fae51132a6703de8f4432c1e68c8432a99
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2016-04-17 18:07:39 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-sudo curl http://download.geonames.org/export/dump/allCountries.zip -O /tmp/allCountries.zip
-sudo unzip allCountries.zip /tmp/.
+cd /tmp && wget http://download.geonames.org/export/dump/allCountries.zip
+#sudo unzip allCountries.zip /tmp/.
 # takes about 20 min an an AWS Medium EC2 Ubuntu 15.04
-sudo -Hu postgres psql -f /home/usrfd/firstdraft/load_geonames.sql dbfd;
+#sudo -Hu postgres psql -f /home/usrfd/firstdraft/load_geonames.sql dbfd;
 
 #python manage.py runscript loadGeoNames
 #python manage.py runscript loadAlternateNames
