6bd50ac2fe844dbeb08fddff4ae71688a873e0bf
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2016-04-16 21:08:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-echo "\n INSTALLING APT PACKAGES \n"
+echo "INSTALLING APT PACKAGES"
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" install apache2 apache2-dev apache2-mpm-prefork apt-file build-essential cmake curl libapache2-mod-wsgi libboost-all-dev libcgal-dev libgdal1-dev libgeos-dev libgmp3-dev libmpfr-dev libmpfr-doc libmpfr4 libmpfr4-dbg libproj-dev libpq-dev python-qgis python-virtualenv qgis vim zip libxslt1-dev
 
-echo "\n CREATING DATABASE \n"
+echo "CREATING DATABASE"
 sudo psql -U postgres -c "CREATE extension postgis"
 sudo psql -U postgres -c "CREATE extension postgis_topology"
 sudo psql -U postgres -c "CREATE extension fuzzystrmatch"
@@ -9,17 +9,23 @@ sudo psql -U postgres -c 'CREATE database dbfd'
 sudo psql -U postgres -c 'CREATE user usrfd'
 sudo psql -U postgres -c 'ALTER DATABASE dbfd OWNER TO usrfd'
 
-echo "\n CREATING user usrfd \n"
+echo "CREATING user usrfd"
+#Create the user named usrfd, who will actually run the Django app.
+#The -m at the end tells it to create a home directory for the usrfd user.
 sudo useradd usrfd -m
 
-echo "\n CLONING firstdraft into /home/usrfd/firstdraft \n"
+echo "CLONING firstdraft into /home/usrfd/firstdraft"
 sudo -u usrfd git clone http://github.com/danieljdufour/firstdraft.git /home/usrfd/firstdraft
+echo "CLONED"
 sudo chown usrfd:usrfd /home/usrfd/firstdraft -R
 
-echo "\n INSTALLING PYTHON PACKAGES \n"
+echo "INSTALLING PYTHON PACKAGES"
 sudo -u usrfd bash -c "cd /home/usrfd && virtualenv venv;"
 sudo -u usrfd bash -c "cd /home/usrfd && source venv/bin/activate && pip install -r /home/usrfd/firstdraft/requirements.txt;"
 
-echo "\n CREATING TABLES \n"
+echo "CREATING TABLES"
 sudo -u usrfd bash -c "cd /home/usrfd && source venv/bin/activate && cd firstdraft/projfd && python manage.py makemigrations"
 sudo -u usrfd bash -c "cd /home/usrfd && source venv/bin/activate && cd firstdraft/projfd && python manage.py migrate"
+
+# create maps directory that will store maps (e.g., geojsons, shapefiles, CSV's)
+sudo -u usrfd bash -c "mkdir /home/usrfd/maps";
