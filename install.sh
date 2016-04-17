a8f07aa09e00e213c1eb9ad7a429ef9aa4c3841e
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2016-04-17 00:03:48 -0700

    reduced verbosity

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "INSTALLING APT PACKAGES"
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qy -o Dpkg::Options::="--force-confnew" install apache2 apache2-dev apache2-mpm-prefork apt-file build-essential cmake curl libapache2-mod-wsgi libboost-all-dev libcgal-dev libgdal1-dev libgeos-dev libgmp3-dev libmpfr-dev libmpfr-doc libmpfr4 libmpfr4-dbg libproj-dev libpq-dev python python-dev python-qgis python-virtualenv qgis vim zip libxslt1-dev
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y -o Dpkg::Options::="--force-confnew" install apache2 apache2-dev apache2-mpm-prefork apt-file build-essential cmake curl libapache2-mod-wsgi libboost-all-dev libcgal-dev libgdal1-dev libgeos-dev libgmp3-dev libmpfr-dev libmpfr-doc libmpfr4 libmpfr4-dbg libproj-dev libpq-dev python python-dev python-qgis python-virtualenv qgis vim zip libxslt1-dev
 
 echo "CREATING DATABASE"
 sudo psql -U postgres -c "CREATE extension postgis"
@@ -26,7 +26,7 @@ sudo -H chown usrfd:usrfd /home/usrfd/firstdraft -R
 echo "INSTALLING PYTHON PACKAGES"
 sudo -H -u usrfd bash -c "cd /home/usrfd && virtualenv /home/usrfd/venv"
 #sudo -H -u usrfd bash -c "cd /home/usrfd && source /home/usrfd/venv/bin/activate && /home/usrfd/venv/bin/pip install -r /home/usrfd/firstdraft/requirements.txt --root /home/usrfd/venv --install-option="--prefix='/home/usrfd/venv/local'""
-sudo -H -u usrfd bash -c "cd /home/usrfd && source /home/usrfd/venv/bin/activate && /home/usrfd/venv/bin/pip install -r /home/usrfd/firstdraft/requirements.txt -vvv"
+sudo -H -u usrfd bash -c "cd /home/usrfd && source /home/usrfd/venv/bin/activate && /home/usrfd/venv/bin/pip install -r /home/usrfd/firstdraft/requirements.txt"
 
 echo "CREATING TABLES"
 sudo -u usrfd bash -c "cd /home/usrfd && source venv/bin/activate && cd firstdraft/projfd && python manage.py makemigrations"
