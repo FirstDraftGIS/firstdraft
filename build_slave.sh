2f399b9ab6bee7f25a95b73a8f25f30c04afb79e
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2016-04-23 18:55:52 -0400

    Update build_slave.sh

--- build_slave.sh
+++ build_slave.sh
@@ -20,16 +20,18 @@ sudo -u postgres psql -c "CREATE DATABASE dbfd;"
 sudo -u postgres psql -c "ALTER DATABASE dbfd OWNER TO usrfd;"
 sudo -u postgres psql -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology; CREATE EXTENSION fuzzystrmatch;" dbfd
 
-echo "CREATING USER usrfd"
-# the m flag means "also create a home folder", which is where we we install the First Draft GIS code
-sudo useradd usrfd -m
+echo "CREATE USER usrfd"
+if [ -z "$(getent passwd usrfd)" ]; then sudo useradd usrfd -m; fi
 
-echo "CLONING firstdraft into /home/usrfd/firstdraft"
-sudo -H -u usrfd bash -c "cd /home/usrfd && git clone https://github.com/DanielJDufour/firstdraft.git"
-sudo -H chown usrfd:usrfd /home/usrfd/firstdraft -R
+if [ ! -d /home/usrfd/firstdraft ]; then
+  echo "CLONING firstdraft into /home/usrfd/firstdraft"
+  sudo -H -u usrfd bash -c "cd /home/usrfd && git clone https://github.com/FirstDraftGIS/firstdraft.git";
+fi
 
-echo "CREATING VIRUTAL ENVIRONMENT"
-sudo -H -u usrfd bash -c "cd /home/usrfd && virtualenv /home/usrfd/venv"
+if [ ! -d /home/usrfd/venv ]; then
+  echo "CREATING VIRUTAL ENVIRONMENT"
+  sudo -H -u usrfd bash -c "cd /home/usrfd && virtualenv /home/usrfd/venv"
+fi
 
 echo "INSTALLING PYTHON PACKAGES"
 sudo -H -u usrfd bash -c "cd /home/usrfd && source /home/usrfd/venv/bin/activate && /home/usrfd/venv/bin/pip install -r /home/usrfd/firstdraft/requirements.txt"
@@ -39,7 +41,8 @@ sudo -Hu usrfd bash -c "source /home/usrfd/venv/bin/activate && python /home/usr
 sudo -Hu usrfd bash -c "source /home/usrfd/venv/bin/activate && python /home/usrfd/firstdraft/projfd/manage.py migrate"
 
 echo "CREATING MAPS FOLDER"
-sudo -u usrfd bash -c "mkdir /home/usrfd/maps";
-
+if [ ! -d /home/usrfd/maps ]; then
+  sudo -u usrfd bash -c "mkdir /home/usrfd/maps";
+fi
 
 echo "FINISHING build_slave.sh"
