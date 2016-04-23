7093c4bf26e6065f075e8ce11dbe5f71d43bb8fc
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2016-04-23 18:11:21 -0400

    Update build_slave.sh

--- build_slave.sh
+++ build_slave.sh
@@ -1,7 +1,9 @@
-echo "starting build_slave.sh"
+echo "STARTING build_slave.sh"
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
+
+echo "INSTALLING SYSTEM PACKAGES"
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" install apache2 apache2-dev apache2-mpm-prefork apt-file build-essential cmake curl default-jdk default-jre fabric git libapache2-mod-wsgi libboost-all-dev libcgal-dev libgdal1-dev libgeos-dev libgmp3-dev libmpfr-dev libmpfr-doc libmpfr4 libmpfr4-dbg libproj-dev libpq-dev maven nodejs npm postgresql postgresql-contrib postgresql-server-dev-all postgresql-9.3-postgis-2.1 python python-dev python-pip python-qgis python-virtualenv qgis vim xvfb zip libxslt1-dev
 
 sudo npm install npm -g;
@@ -9,14 +11,31 @@ sudo npm install npm -g;
 if [ ! -f /usr/bin/node ]; then sudo ln -s /usr/bin/nodejs /usr/bin/node; fi
 
 
-# set up database
+echo "SETTING UP DATABASE"
 sudo service postgresql restart
 sudo -u postgres psql -c "CREATE USER usrfd;";
 sudo -u postgres psql -c "CREATE DATABASE dbfd;"
 sudo -u postgres psql -c "ALTER DATABASE dbfd OWNER TO usrfd;"
 sudo -u postgres psql -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology; CREATE EXTENSION fuzzystrmatch;" dbfd
 
-# create user without password
+echo "CREATING USER usrfd"
+# the m flag means "also create a home folder", which is where we we install the First Draft GIS code
 sudo useradd usrfd -m
 
-echo "finishing build_slave.sh"
+echo "CLONING firstdraft into /home/usrfd/firstdraft"
+sudo -H -u usrfd bash -c "cd /home/usrfd && git clone https://github.com/DanielJDufour/firstdraft.git"
+sudo -H chown usrfd:usrfd /home/usrfd/firstdraft -R
+
+echo "INSTALLING PYTHON PACKAGES"
+sudo -H -u usrfd bash -c "cd /home/usrfd && virtualenv /home/usrfd/venv"
+sudo -H -u usrfd bash -c "cd /home/usrfd && source /home/usrfd/venv/bin/activate && /home/usrfd/venv/bin/pip install -r /home/usrfd/firstdraft/requirements.txt"
+
+echo "CREATING TABLES"
+sudo -Hu usrfd bash -c "source /home/usrfd/venv/bin/activate && python /home/usrfd/firstdraft/projfd/manage.py makemigrations"
+sudo -Hu usrfd bash -c "source /home/usrfd/venv/bin/activate && python /home/usrfd/firstdraft/projfd/manage.py migrate"
+
+echo "CREATING MAPS FOLDER"
+sudo -u usrfd bash -c "mkdir /home/usrfd/maps";
+
+
+echo "FINISHING build_slave.sh"
