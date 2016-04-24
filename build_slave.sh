b9f70c5e1f84d34c0d89f99318b7550776ef34c5
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2016-04-24 00:23:27 -0400

    Now trying to load geonames using python

--- build_slave.sh
+++ build_slave.sh
@@ -42,7 +42,8 @@ echo "LOADING GEONAMES"
 cd /tmp && wget http://download.geonames.org/export/dump/allCountries.zip
 cd /tmp && unzip allCountries.zip
 date
-sudo -u postgres psql -f /home/usrfd/firstdraft/load_geonames.sql dbfd
+#sudo -u postgres psql -f /home/usrfd/firstdraft/load_geonames.sql dbfd
+sudo -Hu usrfd bash -c "cd /home/usrfd/firstdraft/projfd python manage.py runscript loadGeoNames"
 date
 
 echo "SETTING UP APACHE" 
