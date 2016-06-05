399824d2ec64d03e9b5b3e3b70ed17074236e82c
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2016-06-05 13:08:48 -0400

    Update deliver_slave.sh

--- deliver_slave.sh
+++ deliver_slave.sh
@@ -26,7 +26,7 @@ echo "description: $description"
 image_id=$(aws ec2 create-image --instance-id $slave_instance_id --name "$name" --description "$name" | grep -P '(?<="ImageId": ")ami-[a-z\d]+(?=")' --only-matching)
 echo "image_id: $image_id"
 
-echo "waiting until image is created in order to terminate"
+echo "waiting until image is created"
 while true; do
   echo "sleeping 10 seconds"
   sleep 10
@@ -36,6 +36,16 @@ while true; do
     break;
   fi
 done
+
+image_id=$(aws ec2 describe-images --owners self --filters Name=name,Values="First Draft GIS" | grep -P '(?<="ImageId": ")ami-[a-z\d]+(?=")' --only-matching)
+echo "image_id: $image_id"
+if [ "$image_id" == "" ];then
+   echo "image_id is blank"
+else
+    echo "image_id is NOT blank, so deregister"
+    aws ec2 deregister-image --image-id $image_id
+    echo "deregistered"
+fi
 
 aws ec2 terminate-instances --instance-ids $slave_instance_id
 
