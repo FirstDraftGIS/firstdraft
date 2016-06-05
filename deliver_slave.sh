77181ee7404a14ddbd02baeb967173c40879fd4b
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2016-06-05 13:40:36 -0400

    Update deliver_slave.sh

--- deliver_slave.sh
+++ deliver_slave.sh
@@ -2,6 +2,20 @@ echo "starting deliver_slave"
 
 echo "slave_instance_id: $slave_instance_id"
 
+function wait_until_image_is_created {
+  echo "starting wait_until_image_is_created with $1"
+  while true; do
+    echo "    sleeping 10 seconds"
+    sleep 10
+    state=$(aws ec2 describe-images --image-ids $1 | grep -P '(?<="State": ")[a-z]*(?=")' --only-matching)
+    echo "    state: $state"
+    if [ "$state" = "available" ]; then
+      break;
+    fi
+  done
+  echo "finishing wait_until_image_is_created"
+}
+
 aws ec2 stop-instances --instance-ids $slave_instance_id
 
 echo "figuring out version by lookin at previous one"
@@ -27,16 +41,8 @@ description=name
 image_id=$(aws ec2 create-image --instance-id $slave_instance_id --name "$name" --description "$description" | grep -P '(?<="ImageId": ")ami-[a-z\d]+(?=")' --only-matching)
 echo "image_id: $image_id"
 
-echo "waiting until image is created"
-while true; do
-  echo "sleeping 10 seconds"
-  sleep 10
-  state=$(aws ec2 describe-images --image-ids $image_id | grep -P '(?<="State": ")[a-z]*(?=")' --only-matching)
-  echo "state: $state"
-  if [ "$state" = "available" ]; then
-    break;
-  fi
-done
+
+wait_until_image_is_created $image_id
 
 echo "deleting instance because we do not need that anymore"
 aws ec2 terminate-instances --instance-ids $slave_instance_id
@@ -55,6 +61,8 @@ fi
 id_of_public_image=$(aws ec2 copy-image --source-region "us-east-1" --source-image-id "$image_id" --name "First Draft GIS" --description "First Draft GIS automatically creates maps" | grep -P '(?<="ImageId": ")ami-[a-z\d]+(?=")' --only-matching)
 echo "id_of_public_image: $id_of_public_image"
 
+
+wait_until_image_is_created $id_of_public_image
 echo "Making ami public, so everyone can launch First Draft GIS"
 aws ec2 modify-image-attribute --image-id $id_of_public_image --launch-permission "{\"Add\":[{\"Group\":\"all\"}]}"
 
