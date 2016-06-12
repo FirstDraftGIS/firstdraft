e5adda7e9b22af8b117bfc3a85db58ffe5fc520c
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2016-06-12 14:00:21 -0400

    Update test_agent.sh

--- test_agent.sh
+++ test_agent.sh
@@ -3,4 +3,4 @@ echo "Starting test_agent.sh"
 echo "running behave tests"
 sudo --set-home -u usrfd bash -c 'source ~/venv/bin/activate && cd ~/firstdraft/projfd && python ~/firstdraft/projfd/manage.py behave --use-existing-database'
 
-echo "Finishing test_agent.sh
+echo "Finishing test_agent.sh"
