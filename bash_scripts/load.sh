1fd88a1c7e4df9efc3ae26ceba478117f76a81ff
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2016-06-14 21:20:05 -0400

    Update load.sh

--- bash_scripts/load.sh
+++ bash_scripts/load.sh
@@ -15,16 +15,7 @@ sudo --set-home -u usrfd bash -c 'source ~/venv/bin/activate && cd ~/firstdraft/
 echo "LOADING COUNTRY POLYGONS"
 sudo --set-home -u usrfd bash -c 'source ~/venv/bin/activate && cd ~/firstdraft/projfd && python ~/firstdraft/projfd/manage.py runscript loadLSIBWVS'
 
-echo "LOADING MYNAMAR PCODES"
-#sudo --set-home -u usrfd bash -c "source ~/venv/bin/activate && cd ~/firstdraft/projfd && python ~/firstdraft/projfd/manage.py runscript  load --script-args='https://data.hdx.rwlabs.org/dataset/myanmar-adiministrative-boundaries'"
-#sudo --set-home -u usrfd bash -c "source ~/venv/bin/activate && cd ~/firstdraft/projfd && python ~/firstdraft/projfd/manage.py runscript  load --script-args='https://data.hdx.rwlabs.org/dataset/myanmar-village-boundaries'"
-
-#sudo --set-home -u usrfd bash -c "source ~/venv/bin/activate && cd ~/firstdraft/projfd && python ~/firstdraft/projfd/manage.py runscript  load --script-args='https://data.hdx.rwlabs.org/dataset/myanmar-village-locations'"
-#sudo --set-home -u usrfd bash -c "source ~/venv/bin/activate && cd ~/firstdraft/projfd && python ~/firstdraft/projfd/manage.py runscript  load --script-args='https://data.hdx.rwlabs.org/dataset/myanmar-village-boundaries'"
-
-# to-do: figure out admin level for new towns based on looking at parent admin level and see if 100% same admin level
-#sudo -Hu usrfd bash -c "cd /home/usrfd/firstdraft/projfd python manage.py runscript load --script-args='https://data.hdx.rwlabs.org/dataset/myanmar-town-locations'"
-#sudo -Hu usrfd bash -c "cd /home/usrfd/firstdraft/projfd python manage.py runscript load --script-args='https://data.hdx.rwlabs.org/dataset/honduras-admin-level-1-boundaries'"
-
+echo "LOADING OTHER DATASETS"
+sudo --set-home -u usrfd bash -c 'source ~/venv/bin/activate && cd ~/firstdraft/projfd && python ~/firstdraft/projfd/manage.py runscript loadDatasets'
 
 echo "FINISHING load.sh"
