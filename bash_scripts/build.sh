295c54bb386bc9bcc9d0978b147428d0ba60598d
DanielJDufour <daniel.j.dufour@gmail.com>
2016-08-06 18:40:42 +0000

    added pip listing

--- bash_scripts/build.sh
+++ bash_scripts/build.sh
@@ -11,8 +11,9 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confn
 echo "INSTALLING SYSTEM PACKAGES"
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" install apache2 apache2-dev apache2-mpm-prefork apt-file build-essential cmake curl default-jdk default-jre fabric firefox gcc gfortran git libapache2-mod-wsgi libblas3 libboost-all-dev libc6 libcgal-dev libgdal1-dev libgeos-dev libgmp3-dev liblapack3 libmpfr-dev libmpfr-doc libmpfr4 libmpfr4-dbg libproj-dev libpq-dev maven nodejs npm postgresql postgresql-contrib postgresql-server-dev-all '^postgresql-[0-9].[0-9]-postgis-[0-9].[0-9]$' python python-dev python-pip python-qgis python-virtualenv qgis vim xvfb zip libxslt1-dev
 
-echo "INSTALLING PYTHON PACKAGES"
+echo "INSTALLING DJANGO"
 sudo pip install django
+sudo pip list
 
 sudo npm install npm -g;
 # if node command not found link it to existing nodejs command
@@ -72,6 +73,7 @@ sudo -H -u usrfd bash -c "cd /home/usrfd && virtualenv /home/usrfd/venv"
 
 echo "INSTALLING PYTHON PACKAGES"
 sudo -H -u usrfd bash -c "cd /home/usrfd && source /home/usrfd/venv/bin/activate && /home/usrfd/venv/bin/pip install -r /home/usrfd/firstdraft/requirements.txt --quiet --upgrade"
+sudo -H -u usrfd bash -c "cd /home/usrfd && source /home/usrfd/venv/bin/activate && /home/usrfd/venv/bin/pip list"
 
 echo "CREATING TABLES"
 sudo -Hu usrfd bash -c "source /home/usrfd/venv/bin/activate && python /home/usrfd/firstdraft/projfd/manage.py makemigrations"
