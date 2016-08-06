430171da75d8d7172d89a01473f44663f88307b6
DanielJDufour <daniel.j.dufour@gmail.com>
2016-08-06 09:21:43 +0000

    added load alternatenames

--- bash_scripts/load.sh
+++ bash_scripts/load.sh
@@ -12,6 +12,9 @@ sudo python /home/usrfd/firstdraft/loadGeoNames.py
 echo "Updating Primary Key Value Just In Case"
 sudo -u usrfd psql -c "SELECT setval('appfd_place_id_seq', (SELECT MAX(id) FROM appfd_place)+1)" dbfd;
 
+echo "LOADING AlTERNATE NAMES"
+sudo --set-home -u usrfd bash -c 'source ~/venv/bin/activate && cd ~/firstdraft/projfd && python ~/firstdraft/projfd/manage.py runscript loadAlternateNames -v3'
+
 echo "LOADING COUNTRY INFO"
 sudo --set-home -u usrfd bash -c 'source ~/venv/bin/activate && cd ~/firstdraft/projfd && python ~/firstdraft/projfd/manage.py runscript loadCountryInfo -v3'
 
--- projfd/appfd/scripts/loadAlternateNames.py
+++ projfd/appfd/scripts/loadAlternateNames.py
@@ -1,15 +1,17 @@
 from appfd.models import *
 from django.db import connection, transaction
+from django.db.utils import IntegrityError
 from datetime import datetime
 from os.path import isfile
 from urllib import urlretrieve
 from zipfile import ZipFile
+from sys import exit
 
 def run():
     start = datetime.now()
     print "starting loadAlternateNames at ", start
     
-    path_to_dir = '/home/usrfd/data/geonames/'
+    path_to_dir = "/tmp/"
     path_to_zip = path_to_dir + 'alternateNames.zip'
     if not isfile(path_to_zip):
         print "retrieving alternateNames.zip . . . ",
@@ -20,26 +22,60 @@ def run():
             z.extractall(path_to_dir)
         print "done"
 
-    # truncate existing AlternateName table
+    # connect to db
     cursor = connection.cursor()
-    cursor.execute("TRUNCATE appfd_alias CASCADE")
-    cursor.execute("TRUNCATE appfd_aliasplace CASCADE")
 
-    throughs_to_create = []
+    # create stored procedure for loading geonames row
+    cursor.execute("""
+
+    CREATE OR REPLACE FUNCTION 
+
+    loadGeonamesRow(name text, lang text, gid integer)
+    
+    RETURNS void AS '
+
+    DECLARE
+        aliasid int;
+        placeid int;
+
+    BEGIN
+
+    aliasid := (SELECT id FROM appfd_alias WHERE alias = $$name$$ LIMIT 1);
+    IF aliasid IS NULL THEN
+        INSERT INTO appfd_alias (alias, language) VALUES ($$name$$, lang) RETURNING id INTO aliasid;
+    END IF;
+
+    placeid := (SELECT id FROM appfd_place WHERE geonameid = gid);
+
+    IF placeid IS NOT NULL THEN
+
+        INSERT INTO appfd_aliasplace (alias_id, place_id) VALUES (aliasid, placeid);
+
+    END IF;
+
+    END; '
+
+    LANGUAGE PLPGSQL;
+
+    """)
+
+
     with open(path_to_dir + 'alternateNames.txt') as f:
         counter = 0
         for line in f:
             counter += 1
             geonameid, _, language, alternate_name, _, _, _, _ = line.decode("utf-8").split("\t")
-            alias_id = ((Alias.objects.filter(alias=alternate_name) or [None])[0] or Alias.objects.create(alias=alternate_name, language=language)).id
-            place_id = (Place.objects.filter(geonameid=geonameid).values_list("id", flat=True) or [None])[0]
-            if place_id:
-                AliasPlace.objects.get_or_create(alias_id=alias_id, place_id=place_id)
-            if counter % 1000 == 0:
-                print "line: " + str(counter)
+
+            try:
+                cursor.execute(u"SELECT loadGeonamesRow('" + alternate_name + "','" + language + "'," + geonameid + ")")
+            except IntegrityError as e:
+                pass
+
+            if counter % 10000 == 0:
+                print "line " + str(counter) + ": " + str((datetime.now() - start).total_seconds()) + " seconds"
 
     end = datetime.now()
     total_seconds = (end - start).total_seconds()
     print "total_seconds = ", total_seconds
-    with open("log.txt", "a") as f:
+    with open("log.txt", "wb") as f:
         f.write("Loading alternateNames took " + str(total_seconds) + " seconds.")
