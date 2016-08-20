0244cf53cd26dc81acde547ab98d418660350fe7
DanielJDufour <daniel.j.dufour@gmail.com>
2016-08-20 18:09:09 +0000

    try again logging errors in jenkins' testing

--- projfd/features/steps/general.py
+++ projfd/features/steps/general.py
@@ -55,9 +55,9 @@ def we_should_see_a_map(context):
     except Exception as e:
         try:
             print("page_source is")
-            print([context.driver.page_source.decode("utf-8")])
+            print(context.driver.page_source.encode("ascii", "ignore"))
             print("html is")
-            print([context.driver.execute_script("return document.documentElement.innerHTML;").decode("utf-8")])
+            print(context.driver.execute_script("return document.documentElement.innerHTML;").decode("utf-8").encode("ascii","ignore"))
         except Exception as e1:
             print(e1)
         raise e
