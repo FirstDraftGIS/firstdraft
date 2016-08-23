b483b2a3ef93d4cb1c90e00b6da8d1a6a09bb028
DanielJDufour <daniel.j.dufour@gmail.com>
2016-08-23 02:58:53 +0000

    removed old extraneous no longer used files

--- load_alternate_names.sql
+++
@@ -1,3 +0,0 @@
-COPY appfd_alternatename FROM '/tmp/alternateNames.txt' DELIMITER E'\t'
-DROP INDEX IF EXISTS alternatename_index;
-CREATE INDEX alternatename_index ON appfd_alternatename (alternate_name);
