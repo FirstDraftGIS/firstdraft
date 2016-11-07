cb59ed05973999f71fe9dadb0531c11dd3780e98
DanielJDufour <daniel.j.dufour@gmail.com>
2016-11-07 04:05:17 +0000

    added is_country

--- projfd/appfd/scripts/ai/predict.py
+++ projfd/appfd/scripts/ai/predict.py
@@ -18,7 +18,7 @@ from tempfile import mkdtemp
 PATH_TO_DIRECTORY_OF_THIS_FILE = dirname(realpath(__file__))
 MODEL_DIR = PATH_TO_DIRECTORY_OF_THIS_FILE + "/classifier"
 
-CATEGORICAL_COLUMNS = ["admin_level", "country_code", "edit_distance", "has_mpoly", "has_pcode", "is_highest_population", "is_lowest_admin_level", "matches_topic"]
+CATEGORICAL_COLUMNS = ["admin_level", "country_code", "edit_distance", "has_mpoly", "has_pcode", "is_country", "is_highest_population", "is_lowest_admin_level", "matches_topic"]
 CONTINUOUS_COLUMNS = ["cluster_frequency", "country_rank", "median_distance", "population", "popularity"]
 LABEL_COLUMN = "correct"
 COLUMNS = sorted(CATEGORICAL_COLUMNS + CONTINUOUS_COLUMNS) + [LABEL_COLUMN]
@@ -33,6 +33,7 @@ country_rank = real_valued_column("country_rank")
 edit_distance = sparse_column_with_keys(column_name="edit_distance", keys=["0", "1", "2"])
 has_pcode = sparse_column_with_keys(column_name="has_pcode", keys=["True", "False"])
 has_mpoly = sparse_column_with_keys(column_name="has_mpoly", keys=["True", "False"])
+is_country = sparse_column_with_keys(column_name="is_country", keys=["True", "False"])
 is_lowest_admin_level = sparse_column_with_keys(column_name="is_lowest_admin_level", keys=["True", "False"])
 is_highest_population = sparse_column_with_keys(column_name="is_highest_population", keys=["True", "False"])
 matches_topic = sparse_column_with_keys(column_name="matches_topic", keys=["True", "False"])
@@ -50,7 +51,7 @@ admin_level_x_country_code = crossed_column([admin_level, country_code], hash_bu
 #number_of_other_points_in_admin2 = sparse_column_with_hash_bucket("number_of_other_points_in_admin2", hash_bucket_size=1000)
 #feature_columns = [admin_level, cluster_frequency_buckets, country_code, country_rank, edit_distance, is_lowest_admin_level, has_mpoly, has_pcode, matches_topic, median_distance, median_distance_buckets, population_buckets, popularity, admin_level_x_cluster_frequency, admin_level_x_country_code, admin_level_x_median_distance]
 #print "feature_columns:", feature_columns
-wide_columns = [admin_level, cluster_frequency_buckets, country_code, country_rank, edit_distance, is_highest_population, is_lowest_admin_level, has_mpoly, has_pcode, matches_topic, median_distance, median_distance_buckets, population_buckets, popularity, admin_level_x_cluster_frequency, admin_level_x_country_code, admin_level_x_median_distance]
+wide_columns = [admin_level, cluster_frequency_buckets, country_code, country_rank, edit_distance, is_country, is_highest_population, is_lowest_admin_level, has_mpoly, has_pcode, matches_topic, median_distance, median_distance_buckets, population_buckets, popularity, admin_level_x_cluster_frequency, admin_level_x_country_code, admin_level_x_median_distance]
 deep_columns = [
     embedding_column(admin_level, dimension=8),
     cluster_frequency,
@@ -59,6 +60,7 @@ deep_columns = [
     country_rank,
     embedding_column(has_mpoly, dimension=8),
     embedding_column(has_pcode, dimension=8),
+    embedding_column(is_country, dimension=8),
     embedding_column(is_lowest_admin_level, dimension=8),
     embedding_column(is_highest_population, dimension=8),
     median_distance_buckets,
@@ -164,6 +166,7 @@ def train():
                 df_train['country_rank'].append(feature['featureplace__country_rank'] or 999) 
                 df_train['has_mpoly'].append(str(feature['featureplace__place__mpoly'] is not None))
                 df_train['has_pcode'].append(str(feature['featureplace__place__pcode'] is not None)) 
+                df_train['is_country'].append(str(admin_level == 0))
                 df_train['is_lowest_admin_level'].append(str(lowest_admin_level == admin_level))
                 df_train['is_highest_population'].append(str(is_highest_population))
                 df_train['edit_distance'].append("0")
@@ -197,6 +200,7 @@ def train():
                 df_test['country_rank'].append(feature['featureplace__country_rank'] or 999) 
                 df_test['has_mpoly'].append(str(feature['featureplace__place__mpoly'] is not None))
                 df_test['has_pcode'].append(str(feature['featureplace__place__pcode'] is not None)) 
+                df_test['is_country'].append(str(admin_level == 0))
                 df_test['is_lowest_admin_level'].append(str(lowest_admin_level == admin_level))
                 df_test['is_highest_population'].append(str(is_highest_population))
                 df_test['edit_distance'].append("0")
@@ -270,6 +274,7 @@ def run(geoentities):
             df['edit_distance'].append(str(geoentity.edit_distance))
             df['has_mpoly'].append(str(geoentity.has_mpoly or False))
             df['has_pcode'].append(str(geoentity.has_pcode or False))
+            df['is_country'].append(str(admin_level == 0))
             df['is_lowest_admin_level'].append(str(lowest_admin_level == g.admin_level))
             df['is_highest_population'].append(str(is_highest_population))
             df['median_distance'].append(geoentity.median_distance_from_all_other_points)
