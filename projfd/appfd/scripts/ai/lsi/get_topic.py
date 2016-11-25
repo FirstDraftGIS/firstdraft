f1f97bedf9f9a53bb5c48d7ba5b3e3361546f894
DanielJDufour <daniel.j.dufour@gmail.com>
2016-11-25 03:54:06 +0000

    added try catch in LSI get_topic, so won't fail build

--- projfd/appfd/scripts/ai/lsi/get_topic.py
+++ projfd/appfd/scripts/ai/lsi/get_topic.py
@@ -5,16 +5,19 @@ from gensim.models.lsimodel import LsiModel
 from nltk.corpus import stopwords as nltk_stopwords
 from os.path import dirname, realpath
 
-path_to_directory_of_this_file = dirname(realpath(__file__))
+try:
+    path_to_directory_of_this_file = dirname(realpath(__file__))
 
-stopwords = []
-with open(path_to_directory_of_this_file + "/stopwords.txt") as f:
-    stopwords.extend([word for word in f.read().decode("utf-8").split("\n") if word and not word.startswith("#")])   
-stopwords = set(stopwords)
+    stopwords = []
+    with open(path_to_directory_of_this_file + "/stopwords.txt") as f:
+        stopwords.extend([word for word in f.read().decode("utf-8").split("\n") if word and not word.startswith("#")])   
+    stopwords = set(stopwords)
 
-lsi = LsiModel.load(path_to_directory_of_this_file + "/model")
+    lsi = LsiModel.load(path_to_directory_of_this_file + "/model")
    
-dictionary = Dictionary.load(path_to_directory_of_this_file + "/dictionary")
+    dictionary = Dictionary.load(path_to_directory_of_this_file + "/dictionary")
+except Exception as e:
+    print e
 
 def run(text):
 
