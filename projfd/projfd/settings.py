9a300b2bf99ba85f8877a4ed28ba9df228b96f90
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2017-02-12 23:56:06 -0500

    Update settings.py

--- projfd/projfd/settings.py
+++ projfd/projfd/settings.py
@@ -29,8 +29,8 @@ TEMPLATES = [{
             "django.template.context_processors.static",
             "django.template.context_processors.tz",
             "django.contrib.messages.context_processors.messages",
-            'social.apps.django_app.context_processors.backends',
-            'social.apps.django_app.context_processors.login_redirect'
+            "social_django.context_processors.backends",
+            "social_django.context_processors.login_redirect"
         ]
     },
 },]
