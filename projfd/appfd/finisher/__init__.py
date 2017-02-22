42d8de4f17a82b6ea592e8874456071d249f0547
DanielJDufour <daniel.j.dufour@gmail.com>
2017-02-22 01:50:35 +0000

    added finisher

---
+++ projfd/appfd/finisher/__init__.py
@@ -0,0 +1,22 @@
+from appfd.models import Order
+from appfd.scripts import create_csv, create_geojson, create_shapefiles
+from apifd.scripts.create.frequency_geojson import run as create_frequency_geojson
+from multiprocessing import Process
+
+
+def finish_order(key):
+
+    try:
+
+        print "starting finish order with", key
+
+        for _method in create_geojson.run, create_frequency_geojson, create_shapefiles.run, create_csv.run:
+            Process(target=_method, args=(key,)).start()
+
+        from django.db import connection
+        connection.close()
+        order = Order.objects.get(token=key).finish()
+        print "finished order", order
+
+    except Exception as e:
+        print "EXCEPTION in finish_order:", e
