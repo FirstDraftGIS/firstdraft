ce9d0a48c9164dbb164893f613f07149a1b9a0fd
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2017-03-11 14:46:44 -0500

    added python-letsencrypt-apache to build script

--- bash_scripts/build_on_ubuntu_1604.sh
+++ bash_scripts/build_on_ubuntu_1604.sh
@@ -9,7 +9,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confn
 #sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
 
 echo "INSTALLING SYSTEM PACKAGES"
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" install apache2 apache2-dev apt-file build-essential cmake curl cython cython3 default-jdk default-jre fabric firefox gcc gfortran git libapache2-mod-wsgi libatlas-base-dev libblas3 libblas-dev liblapack-dev libboost-all-dev libc6 libcgal-dev libgdal1-dev libgeos-dev libgmp3-dev liblapack3 libmpfr-dev libmpfr-doc libmpfr4 libmpfr4-dbg libopenblas-dev libproj-dev libpq-dev maven nodejs npm postgresql postgresql-contrib postgresql-server-dev-all  '^postgresql-[0-9].[0-9]-postgis-[0-9].[0-9]$' python python3-venv python-dev python-pip python-qgis python-virtualenv qgis subversion vim xvfb zip libxslt1-dev
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" install apache2 apache2-dev apt-file build-essential cmake curl cython cython3 default-jdk default-jre fabric firefox gcc gfortran git libapache2-mod-wsgi libatlas-base-dev libblas3 libblas-dev liblapack-dev libboost-all-dev libc6 libcgal-dev libgdal1-dev libgeos-dev libgmp3-dev liblapack3 libmpfr-dev libmpfr-doc libmpfr4 libmpfr4-dbg libopenblas-dev libproj-dev libpq-dev maven nodejs npm postgresql postgresql-contrib postgresql-server-dev-all  '^postgresql-[0-9].[0-9]-postgis-[0-9].[0-9]$' python python3-venv python-dev python-letsencrypt-apache python-pip python-qgis python-virtualenv qgis subversion vim xvfb zip libxslt1-dev
 
 
 echo "INSTALLING PHANTOMJS"
