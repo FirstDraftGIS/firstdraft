f9cb44fc0bd8282bcdc799b501cc415ddd1056ac
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2017-03-11 15:27:51 -0500

    Create wipe_certs.sh

---
+++ bash_scripts/wipe_certs.sh
@@ -0,0 +1,5 @@
+sudo rm -rf ~/.local/share/letsencrypt
+sudo rm -rf /etc/letsencrypt
+sudo rm -rf /var/log/letsencrypt
+sudo rm -rf /var/lib/letsencrypt
+sudo rm -rf /etc/apache2/sites-available/000-default-le-ssl.conf
