dca0f7bb773a59e7f680af6fc411aa0d2dbb6034
danieljdufour <daniel.j.dufour@gmail.com>
2017-04-01 18:52:15 +0000

    added fields param

--- projfd/apifd/serializers.py
+++ projfd/apifd/serializers.py
@@ -1,13 +1,25 @@
 from appfd.models import Basemap, Place
-from rest_framework.serializers import HyperlinkedModelSerializer
+from drf_queryfields import QueryFieldsMixin
+from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer, SerializerMethodField
 
 # Serializers define the API representation.
-class BasemapSerializer(HyperlinkedModelSerializer):
+class BasemapSerializer(QueryFieldsMixin, ModelSerializer):
     class Meta:
         model = Basemap
         fields = ["id", "name"]
 
-class PlaceSerializer(HyperlinkedModelSerializer):
+class PlaceSerializer(QueryFieldsMixin, ModelSerializer):
+    feature_type = SerializerMethodField()
+
+    def get_feature_type(self, place):
+        lookup = {
+            "PPL": "Populated Place",
+            "PPLA": "Admin 1",
+            "PPLL": "Populated Locality",
+            "ST": "Street"
+        }
+        return lookup.get(place.feature_code, place.feature_code)
+
     class Meta:
         model = Place
-        fields = ["id", "country_code", "name", "point"]
+        fields = ["id", "admin_level", "country_code", "feature_code", "feature_type", "name", "point", "population"]
