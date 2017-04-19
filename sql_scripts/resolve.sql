b306dab936e9cfc57ec4fff45f19b05b6f611da5
danieljdufour <daniel.j.dufour@gmail.com>
2017-04-19 02:33:58 +0000

    added notability to sql resolve custom function

--- sql_scripts/resolve.sql
+++ sql_scripts/resolve.sql
@@ -29,7 +29,8 @@ create type geoentity as (
   has_pcode boolean,
   popularity bigint,
   feature_class varchar,
-  feature_code varchar
+  feature_code varchar,
+  notability int
 );
 
 DROP FUNCTION fdgis_resolve(text[], thorough boolean);
@@ -43,14 +44,14 @@ DECLARE
     most_common_country_code varchar;
 BEGIN
     country_code_rankings := ARRAY(SELECT country_code from appfd_place WHERE name = ANY(names) GROUP BY 1 ORDER BY count(1) DESC);
-    RETURN QUERY SELECT id, admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), name || '--0', name, NULL::varchar, population, point, topic_id, mpoly IS NOT NULL, pcode IS NOT NULL, popularity, feature_class, feature_code as subquery1 FROM appfd_place WHERE name = ANY(names) AND point IS NOT NULL;
-    RETURN QUERY SELECT appfd_place.id, appfd_place.admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), appfd_alias.alias || '--0', appfd_place.name, appfd_alias.alias, appfd_place.population, appfd_place.point, appfd_place.topic_id, appfd_place.mpoly IS NOT NULL, appfd_place.pcode IS NOT NULL, appfd_place.popularity, appfd_place.feature_class, appfd_place.feature_code as subquery2 FROM appfd_place INNER JOIN appfd_aliasplace on (appfd_place.id = appfd_aliasplace.place_id) INNER JOIN appfd_alias ON (appfd_aliasplace.alias_id = appfd_alias.id) WHERE appfd_place.point IS NOT NULL AND appfd_alias.alias = ANY(names);
+    RETURN QUERY SELECT appfd_place.id, admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), name || '--0', name, NULL::varchar, population, point, topic_id, mpoly IS NOT NULL, pcode IS NOT NULL, popularity, feature_class, feature_code, appfd_wikipedia.charcount FROM appfd_place LEFT JOIN appfd_wikipedia ON appfd_place.id = appfd_wikipedia.place_id WHERE name = ANY(names) AND point IS NOT NULL;
+    RETURN QUERY SELECT appfd_place.id, appfd_place.admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), appfd_alias.alias || '--0', appfd_place.name, appfd_alias.alias, appfd_place.population, appfd_place.point, appfd_place.topic_id, appfd_place.mpoly IS NOT NULL, appfd_place.pcode IS NOT NULL, appfd_place.popularity, appfd_place.feature_class, appfd_place.feature_code, appfd_wikipedia.charcount FROM appfd_place INNER JOIN appfd_aliasplace on (appfd_place.id = appfd_aliasplace.place_id) INNER JOIN appfd_alias ON (appfd_aliasplace.alias_id = appfd_alias.id) LEFT JOIN appfd_wikipedia ON appfd_place.id = appfd_wikipedia.place_id WHERE appfd_place.point IS NOT NULL AND appfd_alias.alias = ANY(names);
     most_common_country_code := country_code_rankings[0];
     firstchars := (SELECT ARRAY(SELECT left(name, 1) FROM unnest(names) AS name));
     IF thorough THEN
         RETURN QUERY
-            SELECT id, admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), (SELECT target || '--' || ldist FROM (SELECT target, levenshtein_less_equal(target, appfd_place.name, 2) AS ldist FROM unnest(names::TEXT[]) AS target ORDER BY ldist LIMIT 1) as s1)::text, name, NULL::varchar, population, point, topic_id, mpoly IS NOT NULL, pcode IS NOT NULL, popularity, feature_class, feature_code
-            FROM appfd_place
+            SELECT appfd_place.id, admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), (SELECT target || '--' || ldist FROM (SELECT target, levenshtein_less_equal(target, appfd_place.name, 2) AS ldist FROM unnest(names::TEXT[]) AS target ORDER BY ldist LIMIT 1) as s1)::text, name, NULL::varchar, population, point, topic_id, mpoly IS NOT NULL, pcode IS NOT NULL, popularity, feature_class, feature_code, appfd_wikipedia.charcount
+            FROM appfd_place LEFT JOIN appfd_wikipedia ON appfd_place.id = appfd_wikipedia.place_id
             WHERE country_code = most_common_country_code
             AND appfd_place.point IS NOT NULL
             AND left(name, 1) = ANY(firstchars)
@@ -74,14 +75,14 @@ DECLARE
 BEGIN
     country_codes := ARRAY(SELECT country_code FROM appfd_place WHERE admin_level = 0 AND name = ANY(countries));
     country_code_rankings := ARRAY(SELECT country_code from appfd_place WHERE name = ANY(names) GROUP BY 1 ORDER BY count(1) DESC);
-    RETURN QUERY SELECT id, admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), name || '--0', name, NULL::varchar, population, point, topic_id, mpoly IS NOT NULL, pcode IS NOT NULL, popularity, feature_class, feature_code as subquery1 FROM appfd_place WHERE country_code = ANY(country_codes) AND name = ANY(names) AND point IS NOT NULL;
-    RETURN QUERY SELECT appfd_place.id, appfd_place.admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), appfd_alias.alias || '--0', appfd_place.name, appfd_alias.alias, appfd_place.population, appfd_place.point, appfd_place.topic_id, appfd_place.mpoly IS NOT NULL, appfd_place.pcode IS NOT NULL, appfd_place.popularity, feature_class, feature_code as subquery2 FROM appfd_place INNER JOIN appfd_aliasplace on (appfd_place.id = appfd_aliasplace.place_id) INNER JOIN appfd_alias ON (appfd_aliasplace.alias_id = appfd_alias.id) WHERE appfd_place.country_code = ANY(country_codes) AND appfd_place.point IS NOT NULL AND appfd_alias.alias = ANY(names);
+    RETURN QUERY SELECT appfd_place.id, admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), name || '--0', name, NULL::varchar, population, point, topic_id, mpoly IS NOT NULL, pcode IS NOT NULL, popularity, feature_class, feature_code, appfd_wikipedia.charcount FROM appfd_place LEFT JOIN appfd_wikipedia ON appfd_place.id = appfd_wikipedia.place_id WHERE country_code = ANY(country_codes) AND name = ANY(names) AND point IS NOT NULL;
+    RETURN QUERY SELECT appfd_place.id, appfd_place.admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), appfd_alias.alias || '--0', appfd_place.name, appfd_alias.alias, appfd_place.population, appfd_place.point, appfd_place.topic_id, appfd_place.mpoly IS NOT NULL, appfd_place.pcode IS NOT NULL, appfd_place.popularity, feature_class, feature_code, appfd_wikipedia.charcount FROM appfd_place INNER JOIN appfd_aliasplace on (appfd_place.id = appfd_aliasplace.place_id) INNER JOIN appfd_alias ON (appfd_aliasplace.alias_id = appfd_alias.id) LEFT JOIN appfd_wikipedia ON appfd_place.id = appfd_wikipedia.place_id WHERE appfd_place.country_code = ANY(country_codes) AND appfd_place.point IS NOT NULL AND appfd_alias.alias = ANY(names);
     most_common_country_code := country_code_rankings[0];
     firstchars := (SELECT ARRAY(SELECT left(name, 1) FROM unnest(names) AS name));
     IF thorough THEN
         RETURN QUERY
-            SELECT id, admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), (SELECT target || '--' || ldist FROM (SELECT target, levenshtein_less_equal(target, appfd_place.name, 2) AS ldist FROM unnest(names::TEXT[]) AS target ORDER BY ldist LIMIT 1) as s1)::text, name, NULL::varchar, population, point, topic_id, mpoly IS NOT NULL, pcode IS NOT NULL, popularity, feature_class, feature_code
-            FROM appfd_place
+            SELECT appfd_place.id, admin_level, country_code, admin1_code, indexOf(country_code_rankings, country_code), (SELECT target || '--' || ldist FROM (SELECT target, levenshtein_less_equal(target, appfd_place.name, 2) AS ldist FROM unnest(names::TEXT[]) AS target ORDER BY ldist LIMIT 1) as s1)::text, name, NULL::varchar, population, point, topic_id, mpoly IS NOT NULL, pcode IS NOT NULL, popularity, feature_class, feature_code, appfd_wikipedia.charcount
+            FROM appfd_place LEFT JOIN appfd_wikipedia ON appfd_place.id = appfd_wikipedia.place_id
             WHERE country_code = most_common_country_code
             AND appfd_place.point IS NOT NULL
             AND left(name, 1) = ANY(firstchars)
