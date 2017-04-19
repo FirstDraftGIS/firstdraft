23efb0987d9e1393062a017574f95677f140d3ab
danieljdufour <daniel.j.dufour@gmail.com>
2017-04-19 02:11:15 +0000

    updated initialize wikipedia

--- projfd/appfd/scripts/initialize/wikipedia.py
+++ projfd/appfd/scripts/initialize/wikipedia.py
@@ -1,4 +1,5 @@
 from appfd.models import Place, Wikipedia
+from apifd.serializers import PlaceSerializer
 from broth import Broth
 import csv
 from datetime import datetime
@@ -25,6 +26,9 @@ def run(debug=True):
         if not isfile(path_to_tsv):
             urlretrieve("https://raw.githubusercontent.com/DanielJDufour/geo_notability_assessor/master/notability.tsv", path_to_tsv)
 
+        if debug: print "opening tsv"
+        number_created = 0
+        number_found = 0
         with open(path_to_tsv) as f:
             count = 0
             for row in csv.reader(f, delimiter="\t", quotechar='"'):
@@ -32,6 +36,7 @@ def run(debug=True):
                     count += 1
                     if count != 1:
                         name, latitude, longitude, charcount = row
+                        if debug: print "\n\n\nrow:", row
                         if latitude and longitude:
                             places = Place.objects.filter(name=name)
                             #print "places:", places
@@ -41,29 +46,49 @@ def run(debug=True):
                                 print "e:", e
                                 print "\tlongitude:", longitude
                                 print "\tlatitude:", latitude
-                            found = places.filter(point__distance_lt=(point, Distance(mi=500)))
-                            if not found:
-                                found = places.filter(mpoly__contains=point)
-                            if found.count() > 1:
-                                filtered = found.exclude(population=0)
-                                if filtered:
-                                    found = filtered
-                                if found.count() > 1:
-                                    found = found.exclude(admin_level=None)
+
+                            found_places = places.filter(point__distance_lt=(point, Distance(mi=500)))
+                            if debug: print "found via point distance:", found_places
+
+                            if not found_places:
+                                found_places = places.filter(mpoly__contains=point)
+                                if debug: print "found via mpoly__contains:", found_places
+
+                            if found_places.count() > 1:
+                                found_via_pop = found_places.exclude(population=0).exclude(population=None)
+                                if debug: print "found_via_pop:", found_via_pop
+                                if found_via_pop:
+                                    found_places = found_via_pop
+
+
+                                if found_via_pop.count() > 1:
+                                    matches_with_admin_level = found_places.exclude(admin_level=None)
+                                    if debug: print "found by excluding those without an admin_level:", matches_with_admin_level
+                                    if matches_with_admin_level:
+                                        found_places = matches_with_admin_level
+                                    
                             #    print "found within:", found
-                            #print name, ":", found.values("admin_level","population").count(), ":", charcount
-                            place = found.first()
-                            if not place:
+                            place = found_places.first()
+                            if place:
+                                number_found += 1
+                                if debug: print "found place:", PlaceSerializer(place).data
+                            else:
+                                number_created += 1
                                 place = Place.objects.create(name=name, point=point)
-                                #print "created:", place
-                            Wikipedia.objects.get_or_create(place_id=place.id, charcount=charcount)
-                            #if count > 500:
+                                if debug: print "created place:", place
+                            wikipedia, created = Wikipedia.objects.get_or_create(place_id=place.id, charcount=charcount)
+                            print ("created" if created else "got"), "wikipedia", wikipedia
+                            #if count > 10000:
                             #    break
                 except Exception as e:
                     print e
             
 
-        if debug: print "initializing wikipedia took " + str(str((datetime.now() - start).total_seconds()) / 60) + " seconds"
+        if debug:
+            print "number_created:", number_created
+            print "number_found:", number_found
+            print "found %:", float(number_found) / (number_found + number_created) * 100, "%"
+            print "initializing wikipedia took " + str((datetime.now() - start).total_seconds() / 60) + " minutes"
 
     except Exception as e:
 
