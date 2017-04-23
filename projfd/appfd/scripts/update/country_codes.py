d6db3613a5ab2f86713951fbf0083708331bd3d5
danieljdufour <daniel.j.dufour@gmail.com>
2017-04-23 23:12:31 +0000

    added python script to rapidly update country codes for places that don't have them

---
+++ projfd/appfd/scripts/update/country_codes.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from django.db import connection
+
+def run(debug=False):
+
+    try:
+
+        if debug:
+            print "starting update.country_codes"
+            start = datetime.now()
+
+        connection.cursor().execute("SELECT update_country_codes();")
+
+        if debug:
+            duration_in_seconds = (datetime.now() - start).total_seconds()
+            if duration_in_seconds < 60:
+                if debug: print "updating country_codes took", duration_in_seconds, "seconds"
+            else:
+                if debug: print "updating country_codes took", duration_in_seconds / 60, "minutes"
+
+    except Exception as e:
+
+        print e
