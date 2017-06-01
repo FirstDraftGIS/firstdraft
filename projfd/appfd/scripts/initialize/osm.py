17fd5d5365c631bf17923eee76ed66523d7f8038
danieljdufour <daniel.j.dufour@gmail.com>
2017-06-01 03:48:40 +0000

    initialize osm

--- projfd/appfd/scripts/initialize/osm.py
+++ projfd/appfd/scripts/initialize/osm.py
@@ -10,44 +10,75 @@ from django.contrib.gis.measure import Distance
 from multiprocessing import Process
 from os.path import devnull, isfile
 from os import remove
-from re import match, search, sub
+from re import IGNORECASE, match, search, sub
 from requests import get
 from subprocess import call
 from time import sleep
 from urllib import urlretrieve
 
+def clean_language_name(string):
+    try:
+        if string in ["name_1", "name_2", "name_3", "name_4", "name_5", "name_alt", "name_int", "name_official", "name_old", "name_lang", "name_en", "name1", "name2", "name3", "name4", "name5", "name_translit", "name_historic"]:
+            return "en"
+        elif string in ["name_de"]:
+            return "de"
+        else:
+            string = string.replace("&#39;", "'")
+            return match("name(?:_1|_2|_3|_4|_5|_alt|_int|_official|_old|_lang|_with_accent)?:([^\"<>]*)", string).group(1)
+    except Exception as e:
+        print "exception in clean_language_name:", e
+        print "\tstring:", string
+        raise e
+
+
 def clean_population(string):
-    return match("(?P<pop>\d+)(?: ?\((?P<year>\d+))?", string.replace(",","")).groupdict()['pop']
+    try:
+        if string in ["-"]:
+            return None
+        else:
+            return match("(?:(?:about|around|over) |~|<)?(?P<pop>\d+)(?: ?\((?P<year>\d+))?", string.replace(",","").replace("&#60;","<"), IGNORECASE).groupdict()['pop']
+    except Exception as e:
+        print "exception in clean_population:", e
+        print "\tstring:", string
+        raise e
 
 def run(debug_level=1):
 
     try:
 
-        if debug_level: start = datetime.now()
+      if debug_level: start = datetime.now()
 
-        if debug_level: print "starting to initialize osm"
+      if debug_level: print "starting to initialize osm"
 
-        # start out testing subregion file
-        #url_to_pbf = "https://download.geofabrik.de/north-america/us/district-of-columbia-latest.osm.pbf"
-        url_to_pbf = "http://download.geofabrik.de/north-america-latest.osm.pbf"
+      # start out testing subregion file
+      for name_of_continent in ["africa", "antarctica", "asia", "australia-oceania", "central-america", "europe", "north-america", "south-america"]:
+        print "\n\nname_of_continent:", name_of_continent
+        url_to_pbf = "http://download.geofabrik.de/" + name_of_continent + "-latest.osm.pbf"
+        print "url_to_pbf:", url_to_pbf
         name_of_pbf = url_to_pbf.split("/")[-1]
         if debug_level: print "name_of_pbf: " + name_of_pbf
         path_to_pbf = "/tmp/" + name_of_pbf
-        if not isfile(path_to_pbf):
+        if isfile(path_to_pbf):
+            print path_to_pbf + " already exists"
+        else:
             urlretrieve(url_to_pbf, path_to_pbf)
             if debug_level: print "downloaded:\n\tfrom: " + url_to_pbf + "\n\tto: " + path_to_pbf
+        
 
         # convert from compressed osm.pbf to o5m, which can be used by filter
         path_to_o5m = path_to_pbf.replace(".osm.pbf", ".o5m")
-        #if not isfile(path_to_o5m):
-        if False:
+        if isfile(path_to_o5m):
+            print path_to_o5m + " already exists"
+        else:
             command = "osmconvert " + path_to_pbf + " -o=" + path_to_o5m
             print "about to run: " + command
             call(shlex.split(command))
             print "ran command: " + command
 
         path_to_osm = path_to_o5m.replace(".o5m", ".osm")
-        if not isfile(path_to_osm):
+        if isfile(path_to_osm):
+            print path_to_osm + " already exists"
+        else:
             keep = "place or amenity"
             command = "osmfilter " + path_to_o5m + " --keep='" + keep + "' -o=" + path_to_osm + " --drop-author --drop-ways --drop-relations --drop-version"
             call(shlex.split(command))
@@ -63,15 +94,15 @@ def run(debug_level=1):
         longitude = None
         population = None
         cursor = connection.cursor()
-        #num_tags = 0
-        #num_nodes = 0
+        num_tags = 0
+        num_nodes = 0
         with open(path_to_osm) as f:
             for line in f:
                 count += 1
                 line = line.strip()
                 if debug_level >= 2: print "\n\n\nline:", [line]
                 if line.startswith("<node"):
-                    #num_nodes += 1
+                    num_nodes += 1
                     try:
                         osm_id, latitude, longitude = match(' *<node id="(?P<id>\d+)" lat="(?P<lat>-?\d+(?:\.\d+))" lon="(?P<lon>-?\d+(?:\.\d+))"', line).groups()
                     except Exception as e:
@@ -79,17 +110,18 @@ def run(debug_level=1):
                         print e
 
                 elif line.startswith("<tag"):
-                    #num_tags += 1
+                    num_tags += 1
                     try:
-                        key, value = match(' *<tag k="(?P<key>[^"]+)" v="(?P<value>[^"]+)"', line).groups()
-                        if key.startswith("name"):
-                            if key == "name":
-                                name = value
-                            else:
-                                language = match("name:([a-z]*)", key).group(1)
-                                aliases.append({"alias": name, "language": language})
-                        elif key == "population":
-                            population = clean_population(value.replace(",",""))
+                        key, value = match(' *<tag k="(?P<key>[^"]*)" v="(?P<value>[^"]*)"', line).groups()
+                        if key and value:
+                            if key.startswith("name"):
+                                if key == "name":
+                                    name = value
+                                else:
+                                    language = clean_language_name(key)
+                                    aliases.append({"alias": name, "language": language})
+                            elif key == "population":
+                                population = clean_population(value.replace(",",""))
                             
                     except Exception as e:
                         print "\n", line
@@ -103,11 +135,15 @@ def run(debug_level=1):
                     longitude = None
                     population = None
        
-                #if count >= 50000000000000000000:
+                #if count >= 500000:
                 #    break
 
-        #print "num_nodes:", num_nodes
-        #print "num_tags:", num_tags
+        print "num_nodes:", num_nodes
+        print "num_tags:", num_tags
+
+        if debug_level: print "deleting files to free up disk space"
+        for filepath in [path_to_pbf, path_to_o5m, path_to_osm]:
+            remove(filepath)
 
         cursor.close()
 
