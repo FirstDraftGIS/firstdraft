f7cc2f0e5152c9d056ef91238ee9c9ec3bfeafed
danieljdufour <daniel.j.dufour@gmail.com>
2017-07-09 18:48:15 +0000

    improved if statement and None handling in styler

--- projfd/appfd/styler/__init__.py
+++ projfd/appfd/styler/__init__.py
@@ -8,10 +8,12 @@ def style_order(order_id, style=None):
 
         #basemap_name = style['basemap'] if style and "basemap" in style else "OpenStreetMap.Mapnik"
         #basemap_id = Basemap.objects.get(name=basemap_name).id
-        if style and "basemap" in style:
+        if style and "basemap" in style and style['basemap']:
             map_style_id = MapStyle.objects.create(basemap=style['basemap'])
         else:
+            print "running MapStyle.objects.create()"
             map_style_id = MapStyle.objects.create()
+            print "created mapstyle"
 
         Order.objects.filter(id=order_id).update(style=map_style_id)
 
@@ -19,6 +21,8 @@ def style_order(order_id, style=None):
         for index, feature_id in enumerate(features):
             label = index < 10 # only display label for first 10, should probably randomize later
             Style.objects.create(feature_id=feature_id, label=label)
+
+        print "finishing style with order_id:", order_id
 
     except Exception as e:
         print "[styler.style_order]", e
