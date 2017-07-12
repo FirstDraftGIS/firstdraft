3026e167024bc08bd5a9fd44ec1a327d7f33b470
danieljdufour <daniel.j.dufour@gmail.com>
2017-07-12 12:56:35 +0000

    only execute queries for ai_shared if have run initial migrations

--- projfd/appfd/scripts/ai/ai_shared.py
+++ projfd/appfd/scripts/ai/ai_shared.py
@@ -1,10 +1,15 @@
 from appfd.models import Place
 from django.db import connection
+from django.db.migrations.loader import MigrationLoader
 from tensorflow.contrib.layers import bucketized_column, crossed_column, embedding_column, sparse_column_with_keys, sparse_column_with_hash_bucket, real_valued_column
 
-cursor = connection.cursor()
 
-loose_index_scan = """
+
+if len(MigrationLoader(connection, ignore_no_migrations=True).applied_migrations) > 0:
+
+    cursor = connection.cursor()
+
+    loose_index_scan = """
 WITH RECURSIVE t AS (
     SELECT MIN({0}) AS {0} FROM appfd_place
     UNION ALL
@@ -17,61 +22,89 @@ SELECT NULL WHERE EXISTS(SELECT 1 FROM appfd_place WHERE {0} IS NULL);
 """
 
 
-CATEGORICAL_COLUMNS = ["country_code", "edit_distance", "feature_class", "feature_code", "has_mpoly", "has_pcode", "is_highest_population", "is_lowest_admin_level", "matches_end_user_timezone", "matches_topic", "is_notable"]
-CONTINUOUS_COLUMNS = ["country_rank", "median_distance", "notability", "popularity", "importance"]
-LABEL_COLUMN = "correct"
-COLUMNS = sorted(CATEGORICAL_COLUMNS + CONTINUOUS_COLUMNS) + [LABEL_COLUMN]
+    CATEGORICAL_COLUMNS = ["country_code", "edit_distance", "favor_local", "feature_class", "feature_code", "has_mpoly", "has_pcode", "is_highest_population", "is_lowest_admin_level", "matches_end_user_timezone", "matches_topic", "is_important", "is_notable"]
+    CONTINUOUS_COLUMNS = ["country_rank", "median_distance", "notability", "popularity", "importance"]
+    LABEL_COLUMN = "correct"
+    COLUMNS = sorted(CATEGORICAL_COLUMNS + CONTINUOUS_COLUMNS) + [LABEL_COLUMN]
 
 
-#number_of_other_points_in_country = sparse_column_with_hash_bucket("number_of_other_points_in_country", hash_bucket_size=1000)
-#number_of_other_points_in_admin1 = sparse_column_with_hash_bucket("number_of_other_points_in_admin1", hash_bucket_size=1000)
-#number_of_other_points_in_admin2 = sparse_column_with_hash_bucket("number_of_other_points_in_admin2", hash_bucket_size=1000)
+    #number_of_other_points_in_country = sparse_column_with_hash_bucket("number_of_other_points_in_country", hash_bucket_size=1000)
+    #number_of_other_points_in_admin1 = sparse_column_with_hash_bucket("number_of_other_points_in_admin1", hash_bucket_size=1000)
+    #number_of_other_points_in_admin2 = sparse_column_with_hash_bucket("number_of_other_points_in_admin2", hash_bucket_size=1000)
 
-#admin_level = sparse_column_with_keys(column_name="admin_level", keys=["None","0","1","2","3","4","5","6"]) # I've never seen admin 6, but you never know!
-#cluster_frequency = real_valued_column("cluster_frequency")
-#cluster_frequency_buckets = bucketized_column(cluster_frequency, boundaries=[0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1])
+    #admin_level = sparse_column_with_keys(column_name="admin_level", keys=["None","0","1","2","3","4","5","6"]) # I've never seen admin 6, but you never know!
+    #cluster_frequency = real_valued_column("cluster_frequency")
+    #cluster_frequency_buckets = bucketized_column(cluster_frequency, boundaries=[0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1])
 
-cursor.execute(loose_index_scan.format("country_code"))
-country_codes = sorted(list(set([_tup[0].upper() for _tup in cursor.fetchall() if _tup[0]])))
-country_code = sparse_column_with_keys("country_code", keys=country_codes)
+    cursor.execute(loose_index_scan.format("country_code"))
+    country_codes = sorted(list(set([_tup[0].upper() for _tup in cursor.fetchall() if _tup[0]])))
+    country_code = sparse_column_with_keys("country_code", keys=country_codes)
 
-country_rank = real_valued_column("country_rank")
-edit_distance = sparse_column_with_keys(column_name="edit_distance", keys=["0", "1", "2"])
+    country_rank = real_valued_column("country_rank")
+    edit_distance = sparse_column_with_keys(column_name="edit_distance", keys=["0", "1", "2"])
 
-#feature_classes = list(Place.objects.values_list("feature_class", flat=True).distinct())
-cursor.execute(loose_index_scan.format("feature_class"))
-feature_classes = sorted(list(set([_tup[0].upper() for _tup in cursor.fetchall() if _tup[0]])))
-feature_class = sparse_column_with_keys("feature_class", keys=feature_classes)
+    favor_local = sparse_column_with_keys(column_name="favor_local", keys=["True", "False", "Unknown"])
 
-#feature_codes = list(Place.objects.values_list("feature_code", flat=True).distinct())
-cursor.execute(loose_index_scan.format("feature_code"))
-feature_codes = sorted(list(set([_tup[0].upper() for _tup in cursor.fetchall() if _tup[0]])))
-feature_code = sparse_column_with_keys("feature_code", keys=feature_codes)
+    #feature_classes = list(Place.objects.values_list("feature_class", flat=True).distinct())
+    cursor.execute(loose_index_scan.format("feature_class"))
+    feature_classes = sorted(list(set([_tup[0].upper() for _tup in cursor.fetchall() if _tup[0]])))
+    feature_class = sparse_column_with_keys("feature_class", keys=feature_classes)
 
-has_pcode = sparse_column_with_keys(column_name="has_pcode", keys=["True", "False"])
-has_mpoly = sparse_column_with_keys(column_name="has_mpoly", keys=["True", "False"])
-importance = real_valued_column("importance")
-#is_country = sparse_column_with_keys(column_name="is_country", keys=["True", "False"])
-is_lowest_admin_level = sparse_column_with_keys(column_name="is_lowest_admin_level", keys=["True", "False"])
-is_highest_population = sparse_column_with_keys(column_name="is_highest_population", keys=["True", "False"])
-is_notable = sparse_column_with_keys(column_name="is_notable", keys=["True", "False"]) # does it appear in Wikipedia and have coordinates mentioned in article
-matches_topic = sparse_column_with_keys(column_name="matches_topic", keys=["True", "False"])
-matches_end_user_timezone = sparse_column_with_keys(column_name="matches_end_user_timezone", keys=["True", "False", "Unknown"])
-# probably need a cross column here with timezone and user
-median_distance = real_valued_column("median_distance")
-#median_distance_buckets = bucketized_column(median_distance, boundaries=[10,50,100,200,300])
-notability = real_valued_column("notability") #char count of Wikipedia article
-#population = real_valued_column("population")
-#population_buckets = bucketized_column(population, boundaries=[0, 1, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000])
-popularity = real_valued_column("popularity")
-#admin_level_X_median_distance = crossed_column([admin_level, median_distance_buckets], hash_bucket_size=int(1e4))
-#admin_level_X_cluster_frequency = crossed_column([admin_level, cluster_frequency_buckets], hash_bucket_size=int(1e4))
-#admin_level_X_country_code = crossed_column([admin_level, country_code], hash_bucket_size=int(1e4))
+    #feature_codes = list(Place.objects.values_list("feature_code", flat=True).distinct())
+    cursor.execute(loose_index_scan.format("feature_code"))
+    feature_codes = sorted(list(set([_tup[0].upper() for _tup in cursor.fetchall() if _tup[0]])))
+    feature_code = sparse_column_with_keys("feature_code", keys=feature_codes)
+
+    has_pcode = sparse_column_with_keys(column_name="has_pcode", keys=["True", "False"])
+    has_mpoly = sparse_column_with_keys(column_name="has_mpoly", keys=["True", "False"])
+    importance = real_valued_column("importance")
+    #is_country = sparse_column_with_keys(column_name="is_country", keys=["True", "False"])
+    is_lowest_admin_level = sparse_column_with_keys(column_name="is_lowest_admin_level", keys=["True", "False"])
+    is_highest_population = sparse_column_with_keys(column_name="is_highest_population", keys=["True", "False"])
+    is_notable = sparse_column_with_keys(column_name="is_notable", keys=["True", "False"]) # does it appear in Wikipedia and have coordinates mentioned in article
+    is_important = sparse_column_with_keys(column_name="is_important", keys=["True", "False"]) # defined as OSM names importance of greater than 0.5
+    matches_topic = sparse_column_with_keys(column_name="matches_topic", keys=["True", "False"])
+
+    #cursor.execute(loose_index_scan.format("timezone"))
+    time_zones = list(set(Place.objects.values_list("timezone", flat=True)[:1e5]))
+
+    matches_end_user_timezone = sparse_column_with_keys(column_name="matches_end_user_timezone", keys=["True", "False", "Unknown"])
+    # probably need a cross column here with timezone and user
+    median_distance = real_valued_column("median_distance")
+    #median_distance_buckets = bucketized_column(median_distance, boundaries=[10,50,100,200,300])
+    notability = real_valued_column("notability") #char count of Wikipedia article
+    #population = real_valued_column("population")
+    #population_buckets = bucketized_column(population, boundaries=[0, 1, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000])
+    popularity = real_valued_column("popularity")
+    #admin_level_X_median_distance = crossed_column([admin_level, median_distance_buckets], hash_bucket_size=int(1e4))
+    #admin_level_X_cluster_frequency = crossed_column([admin_level, cluster_frequency_buckets], hash_bucket_size=int(1e4))
+    #admin_level_X_country_code = crossed_column([admin_level, country_code], hash_bucket_size=int(1e4))
 
 
-wide_columns = [country_code, country_rank, edit_distance, feature_code, feature_class, importance, is_highest_population, is_lowest_admin_level, has_mpoly, has_pcode, matches_topic, median_distance, popularity]
-#wide_columns = [is_lowest_admin_level]
-"""
+    wide_columns = [country_code, country_rank, edit_distance, feature_code, feature_class, importance, is_highest_population, is_important, is_lowest_admin_level, has_mpoly, has_pcode, matches_end_user_timezone, matches_topic, median_distance, popularity]
+
+    crossed_columns = []
+    # cross columns
+    #pairs = [("favor_local", col.name) for col in wide_columns]
+    pairs = [("favor_local", "matches_end_user_timezone")]
+    print "pairs:", pairs
+    for name1, name2 in pairs:
+        col1 = locals()[name1]
+        print "col1:", type(col1)
+        col2 = locals()[name2]
+        print "col2:", type(col2)
+        if hasattr(col1, "lookup_config") and hasattr(col2, "lookup_config"):
+            hash_bucket_size = (len(col1.lookup_config.keys) + 1) * (len(col2.lookup_config.keys) + 1)
+            print "hash_bucket_size:", hash_bucket_size
+            new_column = crossed_column([col1, col2], hash_bucket_size=hash_bucket_size)
+            #new_column = crossed_column([col1, col2], hash_bucket_size=1e4)
+            print "new_column:", new_column
+            crossed_columns.append(new_column)
+
+    wide_columns += crossed_columns
+
+
+    """
 deep_columns = [
     embedding_column(admin_level, dimension=8),
     cluster_frequency,
@@ -90,8 +123,8 @@ deep_columns = [
     population_buckets,
     popularity
 ]
-"""
-deep_columns = []
+    """
+    deep_columns = []
 
 def get_df_from_features(features):
 
@@ -130,8 +163,10 @@ def add_features_to_df(df, features):
             lowest_admin_level = -99
 
         if feature['order__end_user_timezone']:
+            favor_local = True
             matches_end_user_timezone = str(feature['featureplace__place__timezone'] == feature['order__end_user_timezone'])
         else:
+            favor_local = False
             matches_end_user_timezone = "Unknown"
 
 
@@ -141,6 +176,7 @@ def add_features_to_df(df, features):
         place_id = feature['featureplace__place_id']
         admin_level = feature['featureplace__place__admin_level']
         importance = feature["featureplace__place__wikipedia__importance"] or 0
+        is_important = str(importance > 0.5)
         notability = feature["featureplace__place__wikipedia__charcount"] or 0
         #df['admin_level'].append(str(admin_level or "None"))
         #df['cluster_frequency'].append(feature['featureplace__cluster_frequency'] or 0)
