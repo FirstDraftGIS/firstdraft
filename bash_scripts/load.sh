b11e3f082aacdfced50ff622ce59cd7ab8ab3f54
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2017-08-08 20:02:44 -0400

    Update load.sh

--- bash_scripts/load.sh
+++ bash_scripts/load.sh
@@ -3,6 +3,9 @@ set -o errexit
 
 echo "STARTING load.sh"
 
+echo "LOADING BASEMAPS"
+sudo --set-home -u usrfd bash -c 'source ~/venv/bin/activate && cd ~/firstdraft/projfd && python ~/firstdraft/projfd/manage.py runscript initialize.basemaps'
+
 echo "LOADING GEONAMES"
 sudo rm -fr /tmp/allCountries.*
 cd /tmp && wget http://download.geonames.org/export/dump/allCountries.zip --no-verbose
