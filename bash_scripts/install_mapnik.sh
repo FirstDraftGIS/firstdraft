7a333722bed8f04a756c221e8b58273727cf4dc9
danieljdufour <daniel.j.dufour@gmail.com>
2018-02-13 05:11:33 +0000

    updated mapnik install

--- bash_scripts/install_mapnik.sh
+++ bash_scripts/install_mapnik.sh
@@ -1,15 +1,12 @@
 # install Mapnik
 # heavily based on https://github.com/mapnik/mapnik/wiki/UbuntuInstallation#ubuntu-1604
 
-# you might have to update your outdated clang
-export CXX="clang++-3.8" && export CC="clang-3.8"
-
 # install mapnik
 git clone https://github.com/mapnik/mapnik ~/mapnik-3.x --depth 10
 cd ~/mapnik-3.x
 git submodule update --init
 source bootstrap.sh
-./configure CUSTOM_CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" CXX=${CXX} CC=${CC}
+./configure CUSTOM_CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" CXX="clang++-3.8" CC="clang-3.8"
 make
 make test
 make install
