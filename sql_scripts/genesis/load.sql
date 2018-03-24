888ab11fc2cf4a843406295825a9486b42027370
DanielJDufour <daniel.j.dufour@gmail.com>
2018-03-24 00:02:36 +0000

    removed genesis sql folder bc unused now

--- sql_scripts/genesis/load.sql
+++
@@ -1,7 +0,0 @@
-/* should only take about 5 minutes */
-DROP TABLE IF EXISTS genesis;
-CREATE TABLE genesis (
-    page_id bigint,
-    titles text
-);
-COPY genesis FROM '/tmp/genesis.tsv' WITH (FORMAT 'csv', DELIMITER E'\t')
